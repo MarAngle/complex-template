edeeedeb56a1b7b304f62b3d7816d9de1bb418f5
MarAngle <yinweiyingzhe@126.com>
2021-07-15 09:12:23 +0800

    2.0.8

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import _func from 'complex-func'
 
-const contents = require.context('./src/data', false, /(\.vue)|(\.jsx)$/)
-const modContents = require.context('./src/mod', false, /(\.vue)|(\.jsx)$/)
+const contents = require.context('./src/data', false, /(\.vue)|(\.js)$/)
+const modContents = require.context('./src/mod', false, /(\.vue)|(\.js)$/)
 
 _func.loadContents(contents, function(item) {
   let data = item.default || item
