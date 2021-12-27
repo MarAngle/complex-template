317a6318f8e6ae1c480a1d35af06ecaca3e2b5c2
MarAngle <yinweiyingzhe@126.com>
2021-12-27 09:38:35 +0800

    添加组件

---
+++ src/mod/ShowValue.vue
@@ -0,0 +1,39 @@
+<template>
+  <span class="complex-show-value" >{{ currentValue }}</span>
+</template>
+<script>
+import _func from 'complex-func'
+
+export default {
+  name: 'ShowValue',
+  data() {
+    return {}
+  },
+  props: {
+    value: {
+      required: false
+    },
+    isExist: {
+      type: Function,
+      required: false,
+      default: _func.isExist
+    },
+    unExistText: {
+      required: false,
+      default: ''
+    },
+    format: {
+      required: false
+    }
+  },
+  computed: {
+    currentValue() {
+      let currentValue = this.format ? this.format(this.value) : this.value
+      if (!this.isExist(currentValue)) {
+        currentValue = this.unExistText
+      }
+      return currentValue
+    }
+  }
+}
+</script>
