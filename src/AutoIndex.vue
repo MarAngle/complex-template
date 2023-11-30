723dd5b4c96113872569ab38d61e64f366ef26b2
Shen GuoQing <yinweiyingzhe@126.com>
2023-11-30 16:15:05 +0800

    初始化

---
+++ src/AutoIndex.vue
@@ -0,0 +1,39 @@
+<style scoped>
+.complex-auto-index{
+  cursor: default;
+}
+</style>
+<template>
+  <span class="complex-auto-index" v-bind="$attrs">{{ currentIndex }}</span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+import { PaginationData } from 'complex-data'
+
+export default defineComponent({
+  name: 'ComplexAutoIndex',
+  props: {
+    index: {
+      type: Number,
+      required: true
+    },
+    pagination: {
+      type: PaginationData,
+      required: false,
+      default: null
+    }
+  },
+  computed: {
+    currentIndex() {
+      let currentIndex = this.index + 1
+      if (this.pagination) {
+        const page = this.pagination.getPage()
+        const size = this.pagination.getSize()
+        currentIndex = currentIndex + (page - 1) * size
+      }
+      return currentIndex
+    }
+  }
+})
+</script>
---
+++ src/InputFile.vue
@@ -0,0 +1,151 @@
+<style lang="less" scoped>
+
+</style>
+<template>
+  <input
+    class="complex-input-file"
+    ref="file"
+    type="file"
+    :accept="accept"
+    :multiple="multiple"
+    :disabled="disabled"
+    :placeholder="placeholder"
+    @change="onChange"
+  />
+</template>
+
+<script lang="ts">
+import { notice } from 'complex-plugin'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'InputFile',
+  props: {
+    accept: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    multiple: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    max: {
+      type: Number,
+      required: false,
+      default: 0
+    },
+    min: {
+      type: Number,
+      required: false,
+      default: 0
+    },
+    append: {
+      type: Boolean,
+      required: false,
+      default: true
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    placeholder: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    size: { // MB
+      type: Number,
+      required: false,
+      default: 0
+    }
+  },
+  methods: {
+    checkAccept(file: File, accept: string) {
+      let isAccept = false
+      if (accept) {
+        const fileType = file.type
+        const namelist = file.name.split('.')
+        const fileName = ('.' + namelist[namelist.length - 1]).toLowerCase()
+        const acceptList = accept.split(',')
+        for (let n in acceptList) {
+          let acceptItem = acceptList[n]
+          if (acceptItem) {
+            if (acceptItem.indexOf('.') === 0) { // 文件后缀形式判断
+              if (acceptItem === fileName) {
+                isAccept = true
+                break
+              }
+            } else { // 文件type判断=暂时只支持image/*...类型
+              if (acceptItem === fileType) {
+                isAccept = true
+                break
+              } else if (fileType && acceptItem.indexOf('*') > -1) {
+                acceptItem = acceptItem.split('*').join('')
+                if (fileType.indexOf(acceptItem) > -1) {
+                  isAccept = true
+                  break
+                }
+              }
+            }
+          }
+        }
+      } else {
+        isAccept = true
+      }
+      return isAccept
+    },
+    check(file: File) {
+      let next = true
+      const isAccept = this.checkAccept(file, this.accept)
+      if (!isAccept) {
+        next = false
+        notice.showMsg( `文件格式不匹配!`, 'error')
+      } else if (this.size) {
+        const currentSize = file.size / 1024 / 1024
+        if (currentSize > this.size) {
+          next = false
+          notice.showMsg( `文件大小不能大于${this.size}MB!`, 'error')
+        }
+      }
+      return next
+    },
+    onChange(e: Event) {
+      const fileList = (e.target as HTMLInputElement).files!
+      if (fileList.length > 0) {
+        if (!this.multiple) {
+          const file = fileList[0]
+          const next = this.check(file)
+          if (next) {
+            this.$emit('file', file)
+          }
+        } else {
+          const currentFileList = []
+          let currentNum = fileList.length
+          if (this.max && currentNum > this.max) {
+            currentNum = this.max
+          }
+          for (let n = 0; n < currentNum; n++) {
+            const file = fileList[n]
+            const next = this.check(file)
+            if (next) {
+              currentFileList.push(file)
+            }
+          }
+          if (currentFileList.length > 0 && currentFileList.length !== currentNum) {
+            // 存在不合格数据
+            if (this.append) {
+              this.$emit('file', currentFileList)
+            }
+          } else {
+            this.$emit('file', currentFileList)
+          }
+        }
+      }
+      (e.target as HTMLInputElement).value = ''
+    }
+  }
+})
+</script>
