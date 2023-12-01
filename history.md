5d24d6468d42d39592e8aae7920b824796e01496
Shen GuoQing <yinweiyingzhe@126.com>
2023-12-01 08:46:41 +0800

    初始化antd

--- history.md
+++ history.md
@@ -1,4 +1,4 @@
 ### ToDo
 
 ### 4.0.1
-- 重构component
+- 基于v4版本的ant-design和complex-component构建模板
--- index.ts
+++ index.ts
@@ -1,11 +1,3 @@
-import AutoIndex from "./src/AutoIndex.vue"
-import HighText from "./src/HighText.vue"
-import InputFile from "./src/InputFile.vue"
-import ShowValue from "./src/ShowValue.vue"
 
 export {
-  AutoIndex,
-  HighText,
-  InputFile,
-  ShowValue
 }
